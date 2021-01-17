995a03896addb56afbbad5949ee3ac3318c34e99
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-17 17:14:11 +0530

    Create lightning.py

---
+++ userbot/lightning.py
@@ -0,0 +1,20 @@
+#kang krne aaye ho  chala ja bhosdike tere kaam ka ni h ye
+#make by Madarchod for chutiapa 🤔😑 you dont kang this 
+#okay to ab isme cheeze bharte h 
+#if you read only this then okay else
+#chala jaa bhosdike 
+from userbot import ALIVE_NAME 
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "The Unknown"
+
+#op for keinshin
+
+The Unknown = "[Unknown](https://t.me/harshharshhuma)"
+OP = "[Sycrox UserBot](https://github.com/TheUnknownKanger/Sycroxbot)"
+OKAY = "[SUPPORT GROUP](https://t.me/LIGHTINGSUPPORT)"
+#itna test h aur aage krte h
+#test successful raha ab aage 
+ALIVE = "SYCROX USERBOT IS ON 🔥 FIRE 🔥" #make by THE UNKNOWN KANGER for Sycrox UserBot
+BOT = " HELLO MASTER MY NAME IS BLACK SYCROX USERBOT I AM A BEST USERBOT 💝"
+EMOJI = "⚡"
+#yrr isko apne bot me aply krne se pehle mere se pooch lena ok
+#aur aage add kruga abhi busy okay 🤔
