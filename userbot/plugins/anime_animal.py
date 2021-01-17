2359cbf0922cbe05488d94f1b7a2f25c1d6dc59b
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-17 18:13:22 +0530

    Add files via upload

---
+++ userbot/plugins/anime_animal.py
@@ -0,0 +1,102 @@
+"""
+MADE BY @Stark_TVL 
+bug fixed by @Melloxd
+"""
+
+from userbot.utils import lightning_cmd
+
+M = (
+    "┈┈╱▔▔▔▔▔▔▔▔▔▔▔▏\n"
+    "┈╱╭▏╮╭┻┻╮╭┻┻╮╭▏ \n"
+    "▕╮╰▏╯┃╭╮┃┃╭╮┃╰▏ \n"
+    "▕╯┈▏┈┗┻┻┛┗┻┻┻╮▏ \n"
+    "▕╭╮▏╮┈┈┈┈┏━━━╯▏\n"
+    "▕╰╯▏╯╰┳┳┳┳┳┳╯╭▏ \n"
+    "▕┈╭▏╭╮┃┗┛┗┛┃┈╰▏ \n"
+    "▕┈╰▏╰╯╰━━━━╯┈┈▏\n"
+)
+
+
+@borg.on(lightning_cmd(pattern=r"spong"))
+async def kek(admin):
+    await borg.edit(M)
+
+    D = (
+        "╭━┳━╭━╭━╮╮\n"
+        "┃┈┈┈┣▅╋▅┫┃\n"
+        "┃┈┃┈╰━╰━━━━━━╮\n"
+        "╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣\n"
+        "╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉\n"
+        "╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤\n"
+        "╲┃┈┈┈┈╭━┳━━━━╯\n"
+        "╲┣━━━━━━┫\n"
+    )
+
+
+@borg.on(lightning_cmd(pattern=r"dog"))
+async def dog(dog):
+    await dog.edit(D)
+    P = (
+        "┈┏━╮╭━┓┈╭━━━━╮\n"
+        "┈┃┏┗┛┓┃╭┫ⓞⓘⓝⓚ┃\n"
+        "┈╰┓▋▋┏╯╯╰━━━━╯\n"
+        "╭━┻╮╲┗━━━━╮╭╮┈\n"
+        "┃▎▎┃╲╲╲╲╲╲┣━╯┈\n"
+        "╰━┳┻▅╯╲╲╲╲┃┈┈┈\n"
+        "┈┈╰━┳┓┏┳┓┏╯┈┈┈\n"
+        "┈┈┈┈┗┻┛┗┻┛┈┈┈┈\n"
+    )
+
+
+F = (
+    " ╱▏┈┈┈┈┈┈▕╲▕╲┈┈┈\n"
+    "▏▏┈┈┈┈┈┈▕▏▔▔╲┈┈\n"
+    "▏╲┈┈┈┈┈┈╱┈▔┈▔╲┈\n"
+    "╲▏▔▔▔▔▔▔╯╯╰┳━━▀\n"
+    "┈▏╯╯╯╯╯╯╯╯╱┃┈┈┈\n"
+    "┈┃┏┳┳━━━┫┣┳┃┈┈┈\n"
+    "┈┃┃┃┃┈┈┈┃┃┃┃┈┈┈\n"
+    "┈┗┛┗┛┈┈┈┗┛┗┛┈┈┈\n"
+)
+
+E = (
+    "┈┈┈┈╱▔▔▔▔▔╲┈╱▔╲\n"
+    "┈┈┈┈▏┈┈▏╭╮▕┈▏╳▕\n"
+    "┈┈┈┈▏┈┈▏┈┈▕┈╲▂╱\n"
+    "┈╱▔▔╲▂╱╲▂▂┈╲▂▏▏\n"
+    "╭▏┈┈┈┈┈┈┈▏╲▂▂╱┈\n"
+    "┃▏┈┈┈┈▏┈┈▏┈┈┈┈┈\n"
+    "╯▏┈╲╱▔╲▅▅▏┈┈┈┈\n"
+    "┈╲▅▅▏▕▔▔▔▔▏┈┈┈┈\n"
+)
+
+H = (
+    "┈┈┈╭━━━━━╮┈┈┈┈┈\n"
+    "┈┈┈┃┊┊┊┊┊┃┈┈┈┈┈\n"
+    "┈┈┈┃┊┊╭━╮┻╮┈┈┈┈\n"
+    "┈┈┈╱╲┊┃▋┃▋┃┈┈┈┈\n"
+    "┈┈╭┻┊┊╰━┻━╮┈┈┈┈\n"
+    "┈┈╰┳┊╭━━━┳╯┈┈┈┈\n"
+    "┈┈┈┃┊┃╰━━┫┈HOMER\n"
+    "┈┈┈┈┈┈┏━┓┈┈┈┈┈┈\n"
+)
+
+
+@borg.on(lightning_cmd(pattern=r"fox"))
+async def fox(fox):
+    await fox.edit(F)
+
+
+@borg.on(lightning_cmd(pattern=r"elephant"))
+async def elephant(elephant):
+    await elephant.edit(E)
+
+
+@borg.on(lightning_cmd(pattern=r"homer"))
+async def homer(homer):
+    await homer.edit(H)
+
+
+@borg.on(lightning_cmd(pattern=r"pig"))
+async def pig(pig):
+    await pig.edit(P)
