a5531c845fa12c9b1d36968d460b8f8608da86b0
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-18 08:35:08 +0530

    Update anilist.py

--- userbot/plugins/anilist.py
+++ userbot/plugins/anilist.py
@@ -10,7 +10,7 @@ import re
 import requests
 
 from userbot import CMD_HELP
-from userbot.utils import lightning_cmd
+from userbot.utils import sycrox_cmd
 
 
 async def callAPI(search_str):
@@ -77,7 +77,7 @@ async def formatJSON(outData):
         return msg
 
 
-@borg.on(lightning_cmd(pattern="anilist ?(.*)"))
+@borg.on(sycrox_cmd(pattern="anilist ?(.*)"))
 async def anilist(event):
     if event.fwd_from:
         return
