c95fc29b65a7c51011acd918a04876cf49f604ec
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-18 08:24:42 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -9,7 +9,7 @@ from telethon import events
 from telethon.tl import functions, types
 
 from userbot import CMD_HELP
-from userbot.utils import lightning_cmd
+from userbot.utils import sycrox_cmd
 
 global USER_AFK  # pylint:disable=E0602
 global afk_time  # pylint:disable=E0602
@@ -54,7 +54,7 @@ async def set_not_afk(event):
                 event.chat_id,
                 "Please set `PRIVATE_GROUP_BOT_API_ID` "
                 + "for the proper functioning of afk functionality "
-                + "Ask In @blacklightningot Chat grp to get help..\n\n `{}`".format(
+                + "Ask In @sycroxsupport Chat grp to get help..\n\n `{}`".format(
                     str(e)
                 ),
                 reply_to=event.message.id,
@@ -131,7 +131,7 @@ async def on_afk(event):
         last_afk_message[event.chat_id] = msg  # pylint:disable=E0602
 
 
-@borg.on(lightning_cmd(pattern=r"afk ?(.*)", outgoing=True))  # pylint:disable=E0602
+@borg.on(sycrox_cmd(pattern=r"afk ?(.*)", outgoing=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
